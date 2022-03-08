9c991f4e7fa9f29a16fa1b72d1f6fd8e3198e1d6
Mitul4212 <100231240+Mitul4212@users.noreply.github.com>
2022-03-08 23:06:05 +0530

    Add files via upload

---
+++ m.1/c5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+{
+    int a;
+    float b;
+    long int c;
+    double d;
+    char e;
+    printf("int:%d\n",sizeof(a));
+    printf("float:%d\n",sizeof(b));
+    printf("long int:%ld\n",sizeof(c));
+    printf("double:%d\n",sizeof(d));
+    printf("char:%d\n",sizeof(e));
+}
